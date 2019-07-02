e7f8f8964792f5b2f5465ea62edd4df7e9feae33
Grant McLendon <grant.mclendon@gmail.com>
2019-07-02 12:01:47 +0200

    Update Config Provider to support Elixir 1.9

--- test/vault_config_provider_test.exs
+++ test/vault_config_provider_test.exs
@@ -6,73 +6,84 @@ defmodule VaultConfigProviderTest do
   setup_with_mocks([
     {Vaultex.Client, [], [read: fn path, _method, _credentials -> {:ok, %{"key" => "ok"}} end]}
   ]) do
-    Application.put_env(:vaultex, :auth, {:method, :credentials})
     :ok
   end
 
   describe "resolve secrets" do
     test "single string path" do
-      assert VaultConfigProvider.resolve_secrets(
-               app: [some_key: "secret:secret/services/my_app key=key"]
-             ) == [app: [some_key: "ok"]]
+      assert [{:app, [some_key: "ok"]} | _] =
+               VaultConfigProvider.resolve_secrets(
+                 app: [some_key: "secret:secret/services/my_app key=key"],
+                 vaultex: [auth: {:method, :credentials}]
+               )
     end
 
     test "keyword path" do
-      assert VaultConfigProvider.resolve_secrets(
-               app: [some_key: [path: "secret/services/my_app", key: "key"]]
-             ) == [app: [some_key: "ok"]]
+      assert [{:app, [some_key: "ok"]} | _] =
+               VaultConfigProvider.resolve_secrets(
+                 app: [some_key: [path: "secret/services/my_app", key: "key"]],
+                 vaultex: [auth: {:method, :credentials}]
+               )
     end
 
     test "key word path with transform" do
-      assert VaultConfigProvider.resolve_secrets(
-               app: [
-                 some_key: [
-                   path: "secret/services/my_app",
-                   key: "key",
-                   fun: &String.upcase/1
-                 ]
-               ]
-             ) == [app: [some_key: "OK"]]
+      assert [{:app, [some_key: "OK"]} | _] =
+               VaultConfigProvider.resolve_secrets(
+                 app: [
+                   some_key: [
+                     path: "secret/services/my_app",
+                     key: "key",
+                     fun: &String.upcase/1
+                   ]
+                 ],
+                 vaultex: [auth: {:method, :credentials}]
+               )
     end
 
     test "keyword paths in array" do
-      assert VaultConfigProvider.resolve_secrets(
-               app: [
-                 some_key: [
-                   [path: "secret/services/my_app", key: "key"],
-                   [path: "secret/services/my_app", key: "key"]
-                 ]
-               ]
-             ) == [app: [some_key: ["ok", "ok"]]]
+      assert [{:app, [some_key: ["ok", "ok"]]} | _] =
+               VaultConfigProvider.resolve_secrets(
+                 app: [
+                   some_key: [
+                     [path: "secret/services/my_app", key: "key"],
+                     [path: "secret/services/my_app", key: "key"]
+                   ]
+                 ],
+                 vaultex: [auth: {:method, :credentials}]
+               )
     end
 
     test "string paths in array" do
-      assert VaultConfigProvider.resolve_secrets(
-               app: [
-                 some_key: [
-                  "secret:secret/services/my_app key=key",
-                  "secret:secret/services/my_app key=key"
-                 ]
-               ]
-             ) == [app: [some_key: ["ok", "ok"]]]
+      assert [{:app, [some_key: ["ok", "ok"]]} | _] =
+               VaultConfigProvider.resolve_secrets(
+                 app: [
+                   some_key: [
+                     "secret:secret/services/my_app key=key",
+                     "secret:secret/services/my_app key=key"
+                   ]
+                 ],
+                 vaultex: [auth: {:method, :credentials}]
+               )
     end
 
     test "deeply nested keyword path" do
-      assert VaultConfigProvider.resolve_secrets(
-               app: [
-                 some_key: [
-                   at: [
-                     a: [
-                       very: [
-                         deep: [
-                           path: [path: "secret/services/my_app", key: "key"]
+      assert [{:app, [some_key: [at: [a: [very: [deep: [path: "ok"]]]]]]} | _] =
+               VaultConfigProvider.resolve_secrets(
+                 app: [
+                   some_key: [
+                     at: [
+                       a: [
+                         very: [
+                           deep: [
+                             path: [path: "secret/services/my_app", key: "key"]
+                           ]
                          ]
                        ]
                      ]
                    ]
-                 ]
-               ]
-             ) == [app: [some_key: [at: [a: [very: [deep: [path: "ok"]]]]]]]
+                 ],
+                 vaultex: [auth: {:method, :credentials}]
+               )
     end
   end
 end
