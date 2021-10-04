e6164302f8182a0782bf48e735f5b0857a916367
Kian Meng Ang <kianmeng.ang@gmail.com>
2021-10-05 03:17:01 +0800

    Misc doc changes (#4)
    * Misc doc changes
    
    Besides other documentation changes, this commit ensures the generated
    HTML doc for HexDocs.pm will become the source of truth for this Elixir
    library and leverage on latest features of ExDoc.
    
    * Update .gitignore
    
    Co-authored-by: Grant McLendon <grant@7mind.de>

--- mix.exs
+++ mix.exs
@@ -1,48 +1,58 @@
 defmodule VaultConfigProvider.MixProject do
   use Mix.Project
 
+  @source_url "https://github.com/sevenmind/vault_config_provider"
+  @version "0.4.0"
+
   def project do
     [
       app: :vault_config_provider,
-      version: "0.4.0",
+      version: @version,
+      name: "VaultConfigProvider",
       elixir: "~> 1.9",
       start_permanent: Mix.env() == :prod,
-      description: "release config provider to read vault secrets",
       package: package(),
       deps: deps(),
-      name: "VaultConfigProvider",
-      docs: [
-        main: "VaultConfigProvider",
-        extras: ["README.md"]
-      ]
+      docs: docs()
     ]
   end
 
-  # Run "mix help compile.app" to learn about applications.
   def application do
     [
       extra_applications: [:logger]
     ]
   end
 
-  # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
       {:vaultex, "~> 1.0"},
-      {:ex_doc, ">= 0.0.0", only: :dev},
+      {:ex_doc, ">= 0.0.0", only: :dev, runtime: false},
       {:mock, "~> 0.3.0", only: :test}
     ]
   end
 
   defp package do
     [
+      description: "Release config provider to read vault secrets.",
       files: ["lib", "mix.exs", "README.md", "LICENSE.md", ".formatter.exs"],
       maintainers: ["Grant McLendon"],
       licenses: ["MIT"],
       links: %{
-        Documentation: "https://hexdocs.pm/vault_config_provider",
-        GitHub: "https://github.com/sevenmind/vault_config_provider"
+        GitHub: @source_url
       }
+    ]
+  end
+
+  defp docs do
+    [
+      extras: [
+        "LICENSE.md": [title: "License"],
+        "README.md": [title: "Overview"]
+      ],
+      main: "readme",
+      source_url: @source_url,
+      source_ref: "v#{@version}",
+      formatters: ["html"]
     ]
   end
 end
